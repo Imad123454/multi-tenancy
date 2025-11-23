5c75d3b42f478aca5c912d9bfd53dff443272d95
Imad123454 <baigimad3@gmail.com>
2025-11-23 17:42:24 +0530

    first commit

--- school_multi/students/admin.py
+++ school_multi/students/admin.py
@@ -4,7 +4,9 @@ from .models import Student
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
     list_display = ['name', 'age', 'tenant']
-    list_filter = ['tenant']  
+    list_filter = ['tenant']  # ye dropdown/sidebar filter add karega
+
+    def get_queryset(self, request):
         qs = super().get_queryset(request)
         if request.user.role == 'master_admin':
             return qs  
